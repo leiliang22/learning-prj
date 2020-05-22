24f01bcd8b04bd29afe7a252581db8fd26ab7ee5
leiliang22 <leiliang835@163.com>
2020-05-22 23:44:54 +0800

    No.105 done.

--- 105.cpp
+++ 105.cpp
@@ -7,42 +7,34 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        size_t l1 = preorder.size();
-        size_t l2 = inorder.size();
-        if (l1 != l2 || l1 == 0) return nullptr;
-
-        return handle(preorder, 0, l1 -1, inorder, 0, l2 - 1);
-    }
-
-    // recursive
-    TreeNode* handle(vector<int>& pre_v, int s1, int e1,
-                vector<int>& in_v, int s2, int e2) {
-        if (e1 == s1) return new TreeNode(pre_v[s1]);
-
-        TreeNode* n = new TreeNode(pre_v[s1]);
-
-        int idx = s2;
-        while (in_v[idx] != pre_v[s1]) ++idx;
-
-        // left
-        int ls1, le1, ls2, le2;
-        ls1 = s1 + 1;
-        le1 = idx;
-        ls2 = s2;
-        le2 = idx - 1;
-        n->left = handle(pre_v, ls1, le1, in_v, ls2, le2);
-
-        // right
-        int rs1, re1, rs2, re2;
-        rs1 = idx + 1;
-        re1 = e1;
-        rs2 = idx + 1;
-        re2 = e2;
-        n->right = handle(pre_v, rs1, re1, in_v, rs2, re2);
-
-        return n;
+        int m = preorder.size();
+        if(m == 0) return NULL;
+        TreeNode* root = new TreeNode(preorder[0]);
+        vector<int> preorder_left, inorder_left, preorder_right, inorder_right;
+        //在inorder里面寻找根节点，划分左子树和右子树
+        int i;
+        //构造左子树的中序遍历
+        for(i = 0; i < m; i ++){
+            if(inorder[i] == preorder[0]) break;
+            inorder_left.push_back(inorder[i]);
+        }
+        //构造右子树的中序遍历
+        for(i = i + 1; i < m; i ++){
+            inorder_right.push_back(inorder[i]);
+        }
+        for(int j = 1; j < m; j ++){
+            //构造左子树的前序遍历
+            if(j <= inorder_left.size())
+                preorder_left.push_back(preorder[j]);
+            //构造右子树的前序遍历
+            else preorder_right.push_back(preorder[j]);
+        }
+        root->left = buildTree(preorder_left, inorder_left);
+        root->right = buildTree(preorder_right, inorder_right);
+        return root;
     }
 };
